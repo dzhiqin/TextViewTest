ea77b0a27d241396b8e1529d5616d1230fa5b25d
dzhiqin <dzhiqin@126.com>
2016-05-06 14:33:33 +0800

    simple calculator

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TextViewTest
 this is a private project for test only
+it is a simple calculator
--- src/com/example/textviewtest/MainActivity.java
+++ src/com/example/textviewtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View.OnFocusChangeListener;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +22,21 @@ public class MainActivity extends Activity {
 	private TextView textView02;
 	private Button alertButton;
 	private int i;
+	private TextView methodTextView;
+	private TextView resultTextView;
+	private EditText value1EditText;
+	private EditText value2EditText;
+	private EditText value4EditText=null;
+	private Button addButton;
+	private Button minusButton;
+	private Button multiplyButton;
+	private Button divisionButton;
+	private Button enterButton;
+	private Double value1;
+	private Double value2;
+	private Double value3;
+	private String str1;
+	private String str2;
 	final int newColor[]=new int []{Color.WHITE,Color.BLUE,Color.CYAN,Color.GREEN,Color.RED};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +47,8 @@ public class MainActivity extends Activity {
 		
 		
 		textView01=(TextView)findViewById(R.id.textview01);
-		textView02=(TextView)findViewById(R.id.textview02);
-		
-		alertButton=(Button)findViewById(R.id.alert_button);
+		textView02=(TextView)findViewById(R.id.textview02);		
+		alertButton=(Button)findViewById(R.id.alert_button);		
 		alertButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -44,7 +60,87 @@ public class MainActivity extends Activity {
 				}
 			
 		});
+		
+		methodTextView=(TextView)findViewById(R.id.methodTextView);
+		resultTextView=(TextView)findViewById(R.id.result);
+		value1EditText=(EditText)findViewById(R.id.value1EditText);
+		value2EditText=(EditText)findViewById(R.id.value2EditText);
+		addButton=(Button)findViewById(R.id.add);
+		minusButton=(Button)findViewById(R.id.minus);
+		multiplyButton=(Button)findViewById(R.id.multiply);
+		divisionButton=(Button)findViewById(R.id.division);
+		enterButton=(Button)findViewById(R.id.enter);
+		
+		addButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				methodTextView.setText("+");
+				
+			}
+		});
+		
+		minusButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				methodTextView.setText("-");
+				
+			}
+		});
+		
+		multiplyButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				methodTextView.setText("*");
+			}
+		});
 
+		divisionButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				methodTextView.setText("/");
+				
+			}
+		});
+		
+		enterButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				if("".equals(value1EditText.getText().toString().trim())){
+					value1=0.0;
+				}else{
+					value1=Double.valueOf(value1EditText.getText().toString());
+				}
+				if("".equals(value2EditText.getText().toString().trim())){
+					value2=0.0;
+				}else{
+					value2=Double.valueOf(value2EditText.getText().toString());
+				}
+		
+				
+				Log.v("MainActivity", value1.toString());
+				Log.v("MainActivity", value2.toString());
+				String str=methodTextView.getText().toString();
+				if("+".equals(str)){
+					value3=value1+value2;
+					resultTextView.setText(value3.toString());
+				}else if("-".equals(str)){
+					value3=value1-value2;
+					resultTextView.setText(value3.toString());
+				}else if("*".equals(str)){
+					value3=value1*value2;
+					resultTextView.setText(value3.toString());
+				}else if("/".equals(str)){
+					value3=value1/value2;
+					resultTextView.setText(value3.toString());
+				}
+			}
+		});
 	}
 
 	@Override
